a7dc53da959299b21e3a980ffebdf3a2f410a1ba
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-07-20 08:09:23 +0200

    Created a loops.php file with examples of loops

---
+++ loops.php
@@ -0,0 +1,39 @@
+<?php
+
+// LOOPS
+// - For
+// - While
+// - Do.. while
+// - Foreach
+
+#For loops ( If you know the no of times it is going to be executed)
+//Parameters: Initializer, Condition, Increment   
+// for($i = 0; $i < 10; $i++){
+//     echo 'Number ' . $i;
+//     echo '<br>';
+//  }
+
+ #While loops (Just condition, rest happens outside)
+ //Parameters: Condition
+//  $i = 0;
+
+//  while($i < 10){
+//     echo $i;
+//     echo '<br>';
+//     $i++;
+//  }
+
+ #Do.. while
+//Parameters: condition
+
+// $i = 0;
+
+// do{
+//     echo $i;
+//     echo '<br>';
+//     $i++;    
+// }
+
+// while($i < 15);
+
+?>
