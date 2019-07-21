d38eb66779bd4026a5ce9d60d18abafef3d72aa0
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-07-21 11:38:02 +0200

    Created website2 folder with files and server superglobal examples

---
+++ website2/server-info.php
@@ -0,0 +1,28 @@
+<?php
+  # $_SERVER SUPERGLOBAL
+
+  // Create Server Array
+
+  $server = [
+    'Host Server Name' => $_SERVER['SERVER_NAME'],
+    'Host Header' => $_SERVER['HTTP_HOST'],
+    'Server Software' => $_SERVER['SERVER_SOFTWARE'],
+    'Document Root' => $_SERVER['DOCUMENT_ROOT'],
+    'Current Page' => $_SERVER['PHP_SELF'],
+    'Script Name' => $_SERVER['SCRIPT_NAME'],
+    'Absolute Path' => $_SERVER['SCRIPT_FILENAME']
+  ];
+
+//   echo $server['Document Root'];
+// print_r($server);
+var_dump($server);
+
+$client = [
+    'Client System' => $_SERVER['HTTP_USER_AGENT'],
+    'Client IP' => $_SERVER['REMOTE_ADDR'],
+    
+];
+
+var_dump($client);
+
+?>
