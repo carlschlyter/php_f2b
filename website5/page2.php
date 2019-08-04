e33ae8cb21a99c5401b60ccd8f70ea40e5f487e6
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-08-04 07:52:15 +0200

    Kind of completed cookies part

---
+++ website5/page2.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_COOKIE['username'])){
+    echo 'User ' . $_COOKIE['username'] . ' is set!<br>';    
+} else {
+    echo 'User not set.';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
